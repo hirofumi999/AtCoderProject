579edf3b4895ffdc72e125adb4beddc3b9532904
hirofumi999 <nack325@yahoo.co.jp>
2022-01-19 22:46:59 +0900

    ABC179E

---
+++ done/ARC114B.py
@@ -0,0 +1,50 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [0] * n
+        self.size = [1] * n
+
+    def find(self, x):
+        if self.par[x] == x:
+            return x
+        else:
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+
+    def same_check(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if self.rank[x] < self.rank[y]:
+            if not self.same_check(x, y):
+                self.size[y] += self.size[x]
+                self.size[x] = 0
+            self.par[x] = y
+        else:
+            if not self.same_check(x, y):
+                self.size[x] += self.size[y]
+                self.size[y] = 0
+            self.par[y] = x
+            if self.rank[x] == self.rank[y]:
+                self.rank[x] += 1
+
+    def siz(self, x):
+        x = self.find(x)
+        return self.size[x]
+
+
+mod = 998244353
+N = int(input())
+A = list(map(int, input().split()))
+UF = UnionFind(N)
+
+
+for i, a in enumerate(A):
+    UF.union(i, a - 1)
+ans = 0
+for i in range(N):
+    if i == UF.find(i):
+        ans += 1
+print(pow(2, ans, mod) - 1)
